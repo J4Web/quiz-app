a2951880c838504ea8958f898783edd66f557926
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-15 23:58:43 +0530

    fixed the level

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,10 +1,12 @@
 import Chip from "@mui/material/Chip";
+import "./level.css";
 function Level(level) {
-  if (level <= 1) return <Chip label="EASY" variant="outlined" />;
+  if (level <= 1)
+    return <Chip className="name" label={"EASY"} variant="outlined" />;
   else if (level >= 1 && level <= 2)
-    return <Chip label="MEDIUM" variant="outlined" />;
+    return <Chip className="name" label="MEDIUM" variant="outlined" />;
   else if (level >= 2 && level <= 3)
-    return <Chip label="HARD" variant="outlined" />;
+    return <Chip className="name" label="HARD" variant="outlined" />;
 }
 
 export default Level;
