d1d1d1451d00c2e0e644c2d0c59fc216dbfb82bf
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-16 23:00:45 +0530

    make the level to filled

--- src/Level.jsx
+++ src/Level.jsx
@@ -2,11 +2,11 @@ import Chip from "@mui/material/Chip";
 import "./level.css";
 function Level(level) {
   if (level <= 1)
-    return <Chip className="name" label={"EASY"} variant="outlined" />;
+    return <Chip className="name" label={"EASY"} variant="filled" />;
   else if (level >= 1 && level <= 2)
-    return <Chip className="name" label="MEDIUM" variant="outlined" />;
+    return <Chip className="name" label="MEDIUM" variant="filled" />;
   else if (level >= 2 && level <= 3)
-    return <Chip className="name" label="HARD" variant="outlined" />;
+    return <Chip className="name" label="HARD" variant="filled" />;
 }
 
 export default Level;
